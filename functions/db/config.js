ae80fc97a6208202400195c05e028ea5b8ca4902
karolis <karolis.stulgys@gmail.com>
2020-03-13 22:28:46 +0200

    change /tmp

--- functions/db/config.js
+++ functions/db/config.js
@@ -3,7 +3,7 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('tmp/database.json');
+const adapter = new FileSync('/tmp/database.json');
 const lowDB = low(adapter);
 
 function randomAmount(min, max) {
