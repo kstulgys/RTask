f42549197e7189bf638849782d456e137d06d686
karolis <karolis.stulgys@gmail.com>
2020-03-13 22:56:18 +0200

    wip

--- functions/db/config.js
+++ functions/db/config.js
@@ -3,7 +3,7 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('./db.json');
+const adapter = new FileSync('./db/db.json');
 const lowDB = low(adapter);
 
 function randomAmount(min, max) {
